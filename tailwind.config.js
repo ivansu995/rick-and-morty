8be1de7fcc53c8fafecee2fdd124ec8b42ceb783
ivansu995 <61602291+ivansu995@users.noreply.github.com>
2023-12-10 12:38:51 +0100

    Clean up and adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+/** @see https://tailwindcss.com/docs/guides/create-react-app */
+
+module.exports = {
+    content: [".scr/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
